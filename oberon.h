37f23a41d0ea252399c040e6915f80fe7d6b03d5
Sergej Chodarev <sergejx@sergejx.net>
2018-07-09 09:56:54 +0200

    Add oberon.h with standard Oberon-0 functions as C macros

---
+++ oberon.h
@@ -0,0 +1,8 @@
+/* Definition of standard Oberon-0 functions */
+
+#include <stdio.h>
+
+#define Read(x) scanf("%d", x)
+#define Write(x) printf("%d", x)
+#define WriteHex(x) printf("%x", x)
+#define WriteLn() printf("\n")
